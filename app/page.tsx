235a2dfa52464e8b9853a08ea6c947b7a6b1a44a
MafinBoy <kloczewiak.filip@gmail.com>
2024-11-07 13:12:05 +0100

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,55 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='flex flex-col items-center py-8 gap-8'>
+      <h1 className='text-5xl font-medium text-orange-400'>Orange Minion</h1>
+      <Feature
+        title='Mastery Showcase'
+        href='/mastery'
+        imageSrc='/masteryPage.png'
+      >
+        Easily view and showcase your champion masteries in a visually appealing
+        layout.
+      </Feature>
+      <Feature
+        title='Shared Match History'
+        href='/commongames'
+        imageSrc='/commonGamesPage.png'
+      >
+        Quickly view all games you have played with another player, allowing you
+        to analyze your shared history and collaboration.
+      </Feature>
+    </div>
+  );
 }
+
+const Feature = ({
+  title,
+  children,
+  imageSrc,
+  href,
+}: {
+  title: string;
+  children: string;
+  imageSrc: string;
+  href: string;
+}) => (
+  <Link
+    className='bg-slate-100 rounded-[2.5rem] max-w-screen-sm p-4 group flex gap-4'
+    href={href}
+  >
+    <div>
+      <h2 className='text-2xl group-hover:underline'>{title}</h2>
+      <p className='mt-2'>{children}</p>
+    </div>
+    <Image
+      className='float-right bg-white p-2 rounded-3xl'
+      src={imageSrc}
+      alt={`Screenshot of ${title}`}
+      width={384}
+      height={384}
+    />
+  </Link>
+);

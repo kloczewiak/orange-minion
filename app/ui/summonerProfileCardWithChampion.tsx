d42d468b699192392eb4d96bd8764497e3ce3f2a
MafinBoy <kloczewiak.filip@gmail.com>
2024-11-21 22:26:00 +0100

    Fix profile card with champion for bots

--- app/ui/summonerProfileCardWithChampion.tsx
+++ app/ui/summonerProfileCardWithChampion.tsx
@@ -4,9 +4,11 @@ import {
   getChampionsLookupTable,
   getChampionTileUrl,
   getSummonerIconUrl,
+  getSwarmChampionLookupTable,
 } from '@/app/lib/helperFunctions';
 import Image from 'next/image';
 import { shimmerStyles } from './components';
+import clsx from 'clsx';
 
 export async function SummonerProfileCardWithChampion({
   puuid,
@@ -17,24 +19,34 @@ export async function SummonerProfileCardWithChampion({
   championId: number;
   region: Region;
 }) {
-  const summoner = await getSummonerDetailsByPUUID(puuid, region);
-  const championLookup = await getChampionsLookupTable();
+  const summoner =
+    puuid !== 'BOT'
+      ? await getSummonerDetailsByPUUID(puuid, region)
+      : undefined;
+  const championLookup = [
+    ...(await getChampionsLookupTable()),
+    ...getSwarmChampionLookupTable(),
+  ];
   const champion = championLookup.find((c) => c.id === championId);
 
   return (
     <div className='relative rounded-xl overflow-hidden w-[150px] h-[150px] shrink-0'>
-      <Image
-        className='top-0 -left-1/4 absolute'
-        src={getSummonerIconUrl(summoner.profileIconId)}
-        alt={`${summoner.gameName}'s icon`}
-        width={150}
-        height={150}
-      />
+      {summoner && (
+        <Image
+          className='top-0 -left-1/4 absolute'
+          src={getSummonerIconUrl(summoner.profileIconId)}
+          alt={`${summoner.gameName}'s icon`}
+          width={150}
+          height={150}
+        />
+      )}
       {champion && (
         <Image
-          className='top-0 -right-1/4 absolute'
+          className={clsx('top-0 absolute', {
+            '-right-1/4': summoner,
+          })}
           style={{
-            clipPath: 'inset(0 0 0 25%)',
+            clipPath: summoner && 'inset(0 0 0 25%)',
           }}
           src={await getChampionTileUrl(championId)}
           alt={champion.name}
@@ -45,9 +57,11 @@ export async function SummonerProfileCardWithChampion({
       <div className='flex flex-col justify-end inset-0 absolute'>
         <div className='flex flex-col text-center leading-4 bg-background/70 py-1'>
           <p className='text-xl font-semibold text-primary leading-5'>
-            {summoner.gameName}
+            {summoner ? summoner.gameName : champion?.name}
           </p>
-          <p className='text-primary-100/80'>#{summoner.tagline}</p>
+          {summoner && (
+            <p className='text-primary-100/80'>#{summoner.tagline}</p>
+          )}
         </div>
       </div>
     </div>
